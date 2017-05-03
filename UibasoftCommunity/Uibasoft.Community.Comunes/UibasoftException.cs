cfeba30596ccf74db2c025c4fe5e81515a69502f
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-05-03 00:38:15 -0400

    implementacion community comunes

---
+++ UibasoftCommunity/Uibasoft.Community.Comunes/UibasoftException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uibasoft.Community.Comunes
+{
+    [Serializable]
+    public class UibasoftException : Exception
+    {
+        public UibasoftException()
+        {
+
+        }
+        public UibasoftException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+        public UibasoftException(string message)
+            : base(message)
+        {
+
+        }
+        public UibasoftException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context)
+        {
+
+        }
+
+    }
+}
