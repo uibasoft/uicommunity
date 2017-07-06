ff9c556dd32030392354aa6026344ad3812ecf8a
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-07-06 09:24:00 -0400

    Implementacion entity dto

---
+++ UibasoftCommunity/Uibasoft.Community.TransferDtos/Entities/EntityDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uibasoft.Community.TransferDtos.Entities
+{
+    public abstract class EntityDto : EntityDto<long>
+    {
+    }
+}
---
+++ UibasoftCommunity/Uibasoft.Community.TransferDtos/Entities/ResultFileDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uibasoft.Community.Comunes.Result;
+
+namespace Uibasoft.Community.TransferDtos.Entities
+{
+    public class ResultFileDto : ResultOperation
+    {
+        public ResultFileDto()
+        {
+
+        }
+        public byte[] Content { get; set; }
+        public string Name { get; set; }
+        public List<string> TypesMime { get; set; }
+
+    }
+}
